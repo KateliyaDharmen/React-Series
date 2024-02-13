3c1abe23772b102042a520913224f17d74c78676
KateliyaDharmen <kateliyadharmen333@gmail.com>
2024-02-13 17:30:24 +0530

    Create react app with basic

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Hello from "./Hello";
+
+function App() {
+  return (
+    <>
+    <h1>Kateliya Dharmen</h1>
+    <Hello />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Hello.js
@@ -0,0 +1,6 @@
+function Hello () {
+    return (
+        <h2>Hello React Js!!</h2>
+    )
+}
+export default Hello
