19ef4bff324f7a727f186a83796f9b20bbb5eda4
KateliyaDharmen <kateliyadharmen333@gmail.com>
2024-03-11 22:59:56 +0530

    Learning Route

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,20 @@
+import Footer from './components/Footer.jsx'
+import Header from './components/Header.jsx'
+import Home from './components/Home.jsx'
+
+function App() {
+  return (
+    <>
+      {/* learn tailwind css(transition, transform, animation) with fun */}
+      {/* <div className='flex gap-5 justify-center'>
+        <h1 className='bg-red-300 rounded-full w-24 my-36 text-8xl hover:bg-red-500 transition ease-in-out delay-75 hover:-translate-y-20 duration-500 animate-bounce'>H</h1>
+        <h1 className='bg-red-300 rounded-full w-24 my-36 text-8xl hover:bg-red-600 transition ease-in-out delay-75 hover:-translate-y-20 duration-500 animate-spin'>E</h1>
+        <h1 className='bg-red-300 rounded-full w-24 my-36 text-8xl hover:bg-red-700 transition ease-in-out delay-75 hover:-translate-y-20 duration-500 animate-ping'>L</h1>
+        <h1 className='bg-red-300 rounded-full w-24 my-36 text-8xl hover:bg-red-800 transition ease-in-out delay-75 hover:-translate-y-20 duration-500 animate-pulse'>L</h1>
+        <h1 className='bg-red-300 rounded-full w-24 my-36 text-8xl hover:bg-red-900 transition ease-in-out delay-75 hover:-translate-y-20 duration-500 animate-bounce'>O</h1>
+      </div> */}
+    </>
+  )
+}
+
+export default App
